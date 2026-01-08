dbfa767d0e77e70b43ae7c9665e184a0b009fd9b
Duy-Kun <sherlockduy2001@gmail.com>
2026-01-08 01:46:27 +0100

    dev: completed first challenge

--- Algorithms/1.py
+++ Algorithms/1.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+# 1. Two Sum
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+#You can return the answer in any order.
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        listReturn = []
+
+        
+        for i in range(len(nums)):
+            fixNum = target - nums[i]
+            
+            for j in range(i+1, len(nums)):
+                if nums[j] == fixNum:
+                    listReturn.append(i)
+                    listReturn.append(j)
+                    return listReturn
